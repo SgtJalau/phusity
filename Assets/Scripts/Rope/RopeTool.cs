14ba1de0a8f305e6cb4abf77c775d20007c455cf
OlesenJonas <45714731+OlesenJonas@users.noreply.github.com>
2020-12-01 00:37:33 +0100

    Dynamic Ropes
    first version of dynamic ropes

--- Assets/Scripts/Rope/RopeTool.cs
+++ Assets/Scripts/Rope/RopeTool.cs
@@ -6,8 +6,7 @@ enum ROPE_STATE
 {
     NONE,
     ONE_STATIC,
-    ONE_DYNAMIC,
-    ACTIVE
+    ONE_DYNAMIC
 }
 
 public class RopeTool : MonoBehaviour
@@ -16,11 +15,13 @@ public class RopeTool : MonoBehaviour
     public Material mat;
     public GameObject ropePrefab;
 
-    LayerMask layerMask;
     GameObject debugSphere = null;
+
+    LayerMask layerMask;
     ROPE_STATE state = ROPE_STATE.NONE;
-    Vector3 p1;
-    Vector3 p2;
+    RaycastHit hit1;
+    RaycastHit hit2;
+    SpringJoint activeJoint = null;
 
     // Start is called before the first frame update
     void Start()
@@ -38,24 +39,40 @@ public class RopeTool : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        //while(Input.GetMouseButton(0))
         {
             Vector3 wPos = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth*0.5f, cam.pixelHeight*0.5f, 0));
             RaycastHit result;
             if (Physics.SphereCast(origin: wPos, 0.1f, transform.forward, out result, Mathf.Infinity, layerMask))
             {
+                bool hitDynamic = result.collider.CompareTag("DynamicRopeTarget");
                 switch (state)
                 {
                     case ROPE_STATE.NONE:
-                        p1 = result.point;
-                        state = ROPE_STATE.ONE_STATIC;
+                        hit1 = result;
+                        state = hitDynamic ? ROPE_STATE.ONE_DYNAMIC : ROPE_STATE.ONE_STATIC;
                         break;
                     case ROPE_STATE.ONE_STATIC:
-                        p2 = result.point;
-                        if (!Physics.Raycast(p1, p2, Mathf.Infinity, ~layerMask)) //maybe SphereCast
+                        hit2 = result;
+                        if (hitDynamic)
                         {
-                            GameObject rope = Instantiate(ropePrefab, (p1 + p2) * 0.5f, Quaternion.LookRotation(p2 - p1, Vector3.up));
-                            rope.transform.localScale = new Vector3(1, 1, (p2 - p1).magnitude);
+                            createDynamicRope();
+                        }
+                        else
+                        {
+                            createStaticRope();   
+                        }
+                        state = ROPE_STATE.NONE;
+                        break;
+                    case ROPE_STATE.ONE_DYNAMIC:
+                        if (hitDynamic)
+                        {
+                            state = ROPE_STATE.NONE;
+                        }
+                        else
+                        {
+                            hit2 = hit1;
+                            hit1 = result;
+                            createDynamicRope();
                         }
                         state = ROPE_STATE.NONE;
                         break;
@@ -64,5 +81,32 @@ public class RopeTool : MonoBehaviour
                 debugSphere.transform.position = result.point;
             }
         }
+    }
+
+    void createStaticRope()
+    {
+        if (!Physics.Raycast(hit1.point, hit2.point, Mathf.Infinity, ~layerMask)) //maybe SphereCast
+        {
+            GameObject rope = Instantiate(ropePrefab, (hit1.point + hit2.point) * 0.5f, Quaternion.LookRotation(hit2.point - hit1.point, Vector3.up));
+            rope.transform.localScale = new Vector3(1, 1, (hit2.point - hit1.point).magnitude);
+        }
+    }
+
+    void createDynamicRope()
+    {
+        if (activeJoint != null) //destroy currently active connection
+        {
+            Destroy(activeJoint);
+        }
+        GameObject dynamicObjectRoot = hit2.transform.gameObject;
+        activeJoint = dynamicObjectRoot.AddComponent<SpringJoint>();
+        activeJoint.connectedBody = hit1.rigidbody; //TODO: could be null (although all rope targets should have a rigidbody)
+        activeJoint.autoConfigureConnectedAnchor = false;
+        activeJoint.connectedAnchor = Vector3.zero;
+        activeJoint.anchor = dynamicObjectRoot.transform.GetChild(2).localPosition; //TODO: Dont just refernce by hierarchy index
+        activeJoint.spring = 100;
+        activeJoint.damper = 1.0f;
+        activeJoint.minDistance = 0.0f;
+        activeJoint.maxDistance = (hit1.point - hit2.point).magnitude+1; //TODO: offset as parameter?
     }
 }
