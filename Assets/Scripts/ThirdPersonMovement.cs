87bf0e73659b7dd7355604ef63da4ca068086473
Benebaby <benedikt.krieger71@gmail.com>
2020-12-01 16:03:43 +0100

    added ThirdPersonMovement
    Cinemachine added;
    ThirdpersonMOvement script added
    and CursorLock added

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform camTransform;
+    public float speed = 6f;
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
+
+    public float turnSmoothTime = 0.1f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private float turnSmoothVelocity;
+    private Vector3 velocity = new Vector3(0f, -9.81f, 0f);
+    private bool isGrounded;
+    private bool doubleJump = false;
+    private float currentSpeed;
+
+    void Start() 
+    {
+        currentSpeed = speed;
+    }
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * currentSpeed * Time.deltaTime);
+        }
+        
+        if(doubleJump)
+        {
+            if(Input.GetButtonDown("Jump"))
+            {
+                doubleJump = false;
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+        if (isGrounded)
+        {
+            currentSpeed = speed;
+            doubleJump = false;
+            if(velocity.y < 0)
+            {
+                velocity.y = -9.81f;
+            }
+            if(Input.GetButtonDown("Jump"))
+            {
+                doubleJump = true;
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+        }
+        else
+        {
+            if(currentSpeed > (speed/2.0f))
+                currentSpeed -= (speed/2.0f) * Time.deltaTime;
+            velocity.y += gravity * Time.deltaTime;
+        }
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
