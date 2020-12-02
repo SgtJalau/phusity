53d253d8a53d47014ad0656c0ad2a4b39649fb96
Benebaby <benedikt.krieger71@gmail.com>
2020-12-02 10:35:25 +0100

    Update ThirdPersonMovement.cs
    Debugging Third Person Player Movement script

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -16,14 +16,21 @@ public class ThirdPersonMovement : MonoBehaviour
     public LayerMask groundMask;
 
     private float turnSmoothVelocity;
-    private Vector3 velocity = new Vector3(0f, -9.81f, 0f);
+    private float dashSmoothVelocity;
+    private Vector3 velocity;
     private bool isGrounded;
-    private bool doubleJump = false;
+    private bool doubleJump;
+    private bool dashready;
+    private bool dashactive;
     private float currentSpeed;
 
     void Start() 
     {
         currentSpeed = speed;
+        velocity = new Vector3(0f, -9.81f, 0f);
+        doubleJump = false;
+        dashready = true;
+        dashactive = false;
     }
     void Update()
     {
@@ -32,15 +39,37 @@ public class ThirdPersonMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 moveDir = new Vector3(0f, 0f, 0f);
 
-        if(direction.magnitude >= 0.1f)
+        if(direction.magnitude >= 0.1f && !dashactive)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * currentSpeed * Time.deltaTime);
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            velocity.x = moveDir.normalized.x * currentSpeed;
+            velocity.z = moveDir.normalized.z * currentSpeed;
+        }else{
+            velocity.x = 0f;
+            velocity.z = 0f;
+        }
+
+        if(dashready && !dashactive){
+            if(Input.GetKeyDown(KeyCode.LeftShift)){
+                velocity.x *= 10f;
+                velocity.z *= 10f;
+                dashready = false;
+                dashactive = true;
+            }
+        }
+        if(dashactive &&  new Vector2(velocity.x, velocity.z).magnitude > 0.1f){
+            velocity.x =  Mathf.SmoothDamp(velocity.x, 0f, ref dashSmoothVelocity, 0.3f);
+            velocity.z =  Mathf.SmoothDamp(velocity.z, 0f,ref dashSmoothVelocity, 0.3f);
+        }
+        else if(dashactive){
+            dashactive = false;
+            dashready = true;
         }
         
         if(doubleJump)
@@ -71,6 +100,8 @@ public class ThirdPersonMovement : MonoBehaviour
                 currentSpeed -= (speed/2.0f) * Time.deltaTime;
             velocity.y += gravity * Time.deltaTime;
         }
+
+
         controller.Move(velocity * Time.deltaTime);
     }
 }
