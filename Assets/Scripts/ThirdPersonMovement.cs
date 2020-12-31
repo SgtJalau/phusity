3d0269f9d638e83ad97db68f2c7490dce3858c83
BenediktKrieger <benediktkrieger@uni-koblenz.de>
2020-12-31 18:12:14 +0100

    Added Custom Gravity to Character and Doublejump

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -8,14 +8,18 @@ public class ThirdPersonMovement : MonoBehaviour
     public Transform camTransform;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
+    public float gravityMultiplyer = 1.0f;
+    public float jumpHeight = 6.0f;
 
     private bool jump;
+    private bool doubleJump;
     private bool dash;
     private float turnSmoothVelocity;
     private Vector3 velocity;
     private bool isGrounded;
     private Vector3 direction;
     private float currentSpeed;
+    private float doublejumpTimeout;
 
     private float lastDash;
     
@@ -24,14 +28,17 @@ public class ThirdPersonMovement : MonoBehaviour
         velocity = new Vector3(0f, 0f, 0f);
         direction = new Vector3(0f, 0f, 0f);
         jump = true;
+        doubleJump = false;
         dash = true;
         isGrounded = false;
         lastDash = Time.realtimeSinceStartup;
         currentSpeed = speed;
+        doublejumpTimeout = 0.2f;
     }
 
     void OnCollisionEnter(Collision collision){
         jump = true;
+        doubleJump = false;
         isGrounded = true;
     }
 
@@ -50,6 +57,11 @@ public class ThirdPersonMovement : MonoBehaviour
         direction = new Vector3(horizontal, 0f, vertical).normalized;
         Vector3 moveDir = new Vector3(0f, 0f, 0f);
 
+        if(doublejumpTimeout > 0.0f){
+            doublejumpTimeout -= Time.deltaTime;
+        }
+
+        rb.AddForce(Physics.gravity * gravityMultiplyer, ForceMode.Acceleration);
         if(direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
@@ -65,8 +77,15 @@ public class ThirdPersonMovement : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.Space) && jump && isGrounded)
         {
-            rb.AddForce(Vector3.up * 10.0f, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f *  Physics.gravity.y * gravityMultiplyer), ForceMode.Impulse);
             jump = false;
+            doubleJump = true;
+            doublejumpTimeout = 0.2f;
+        }else if (Input.GetKey(KeyCode.Space) && doubleJump && !isGrounded && doublejumpTimeout <= 0.0f)
+        {
+            rb.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2f *  Physics.gravity.y * gravityMultiplyer), ForceMode.Impulse);
+            jump = false;
+            doubleJump = false;
         }
         velocity.y = rb.velocity.y;
 
