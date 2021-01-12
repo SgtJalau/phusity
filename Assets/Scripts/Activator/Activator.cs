699a708a79c926f4eabb0a52e9672def9a31d27a
OlesenJonas <45714731+OlesenJonas@users.noreply.github.com>
2021-01-12 01:31:13 +0100

    added Activator and Activatable classes for levers / buttons etc

---
+++ Assets/Scripts/Activator/Activator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public List<Activatable> activatableTargets = new List<Activatable>();
+
+    public void activate()
+    {
+        foreach (Activatable target in activatableTargets)
+            target.activate();
+    }
+
+    public void deactivate()
+    {
+        foreach (Activatable target in activatableTargets)
+            target.deactivate();
+    }
+}
