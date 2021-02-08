755987cfa432422228fcec8b7f18bb553b63a37e
OlesenJonas <45714731+OlesenJonas@users.noreply.github.com>
2021-02-08 14:22:08 +0100

    added some comments

--- Assets/Scripts/TargetingMode/TargetingActivator.cs
+++ Assets/Scripts/TargetingMode/TargetingActivator.cs
@@ -62,19 +62,19 @@ public class TargetingActivator : MonoBehaviour
             Vector3 camXWS = Camera.main.transform.right;
             Vector3 camYWS = Camera.main.transform.up;
 
-            //--------------------------- "FINDING" TARGETS AND DRAWING OVERLAY ELEMENT FOR EACH --------------------------------//
-
-            //test if we are currently aiming at a target
+            //test if we are currently aiming at a something
             Camera cam = Camera.main;
             Vector3 wPos = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth * 0.5f, cam.pixelHeight * 0.5f, 0));
             RaycastHit result;
             Collider hitCollider = null;
-            //TODO: could store last hit and pass it to the RopeTool somehow -> dont need to SphereCast twice
+            //only testing against RopeTargets for now, can add more layers
+            //TODO: store last hit in class field and access it from outside (ie RopeTool doesnt need to SphereCast again, just use the result from here)
             if (Physics.SphereCast(origin: wPos, 0.1f, transform.forward, out result, Mathf.Infinity, LayerMask.GetMask("RopeTarget")))
             {
                 hitCollider = result.collider;
             }
 
+            //--------------------------- "FINDING" ROPETARGETS AND DRAWING OVERLAY ELEMENT FOR EACH --------------------------------//
             var hits = Physics.OverlapSphere(transform.position, 100.0f, LayerMask.GetMask("RopeTarget"));
             Matrix4x4[] matrices = new Matrix4x4[hits.Length];
             int amount = 0;
@@ -85,7 +85,7 @@ public class TargetingActivator : MonoBehaviour
                     currentState == RopeToolState.SELECTED_STATIC ||
                     (currentState == RopeToolState.SELECTED_DYNAMIC && !hits[i].CompareTag("DynamicRopeTarget")))
                 {
-                    //unlike raycasthit.transform, sphereoverlap returns the colliders own. 
+                    //unlike raycasthit.transform, sphereoverlap returns the colliders own transform. 
                     //Ie we have to get the parent containing the rigidbody and its transform ourselves
                     GameObject topmostGameObject = hits[i].attachedRigidbody.gameObject;
 
@@ -97,18 +97,18 @@ public class TargetingActivator : MonoBehaviour
                     { 
                         rotMat *= Matrix4x4.Rotate(Quaternion.Euler(Time.time*360, 0, 0));
                     }
-                    //the following can be avoided if we require the "TargetPosition" Empty to not also define the target collider
                     Transform targetTransform = topmostGameObject.transform.Find("TargetPosition");
 
                     matrices[amount] = Matrix4x4.Translate(targetTransform.position) * rotMat;
                     amount++;
                 }
             }
+            //Drawing the generated overlay
             Graphics.DrawMeshInstanced(simpleQuad, 0, ropeTargetMat, matrices, amount,
                 properties: null,
                 castShadows: UnityEngine.Rendering.ShadowCastingMode.Off,
                 receiveShadows: false,
-                layer: LayerMask.NameToLayer("UI"), //may break stuff, will see when we have actual UI elements
+                layer: LayerMask.NameToLayer("UI"), //Dk if this takes precedence over the material settings. May break stuff, will see when we have actual UI elements
                 camera: null,
                 lightProbeUsage: UnityEngine.Rendering.LightProbeUsage.Off);
         }
